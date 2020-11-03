f5fe45df4652dc38fe3d47bfd4b9b1b015a385ee
Peter Loos <peter.loos@gmx.de>
2020-11-03 10:38:25 +0100

    moved 'dining philosophers' to project 'parallel programming'

--- Spreadsheet/Spreadsheetcell.h
+++ Spreadsheet/Spreadsheetcell.h
@@ -7,28 +7,28 @@
 //class SpreadsheetCell
 //{
 //public:
-//	SpreadsheetCell() = default;
-//	SpreadsheetCell(double initialValue);
-//	explicit SpreadsheetCell(std::string_view initialValue);
+//    SpreadsheetCell() = default;
+//    SpreadsheetCell(double initialValue);
+//    explicit SpreadsheetCell(std::string_view initialValue);
 //
-//	void set(double inValue);
-//	void set(std::string_view inString);
+//    void set(double inValue);
+//    void set(std::string_view inString);
 //
-//	double getValue() const { return mValue; }
-//	std::string getString() const { return doubleToString(mValue); }
+//    double getValue() const { return mValue; }
+//    std::string getString() const { return doubleToString(mValue); }
 //
-//	static std::string doubleToString(double inValue);
-//	static double stringToDouble(std::string_view inString);
+//    static std::string doubleToString(double inValue);
+//    static double stringToDouble(std::string_view inString);
 //
-//	SpreadsheetCell& operator+=(const SpreadsheetCell& rhs);
-//	SpreadsheetCell& operator-=(const SpreadsheetCell& rhs);
-//	SpreadsheetCell& operator*=(const SpreadsheetCell& rhs);
-//	SpreadsheetCell& operator/=(const SpreadsheetCell& rhs);
+//    SpreadsheetCell& operator+=(const SpreadsheetCell& rhs);
+//    SpreadsheetCell& operator-=(const SpreadsheetCell& rhs);
+//    SpreadsheetCell& operator*=(const SpreadsheetCell& rhs);
+//    SpreadsheetCell& operator/=(const SpreadsheetCell& rhs);
 //
 //private:
-//	std::variant<int, float, std::string> m_value;
-//	//double mValue = 0;
-//	//mutable size_t mNumAccesses = 0;
+//    std::variant<int, float, std::string> m_value;
+//    //double mValue = 0;
+//    //mutable size_t mNumAccesses = 0;
 //};
 //
 //SpreadsheetCell operator+(const SpreadsheetCell& lhs, const SpreadsheetCell& rhs);
