32b2efe8b389400d5674f098aa818c65f13f45fa
Peter Loos <peter.loos@gmx.de>
2025-12-22 18:58:50 +0100

    continued work

--- CaseStudies/CopyOnWrite/LoremIpsum.h
+++ CaseStudies/CopyOnWrite/LoremIpsum.h
@@ -5,26 +5,26 @@
 
 #pragma once
 
-#include <cstddef>      // std::size_t
-#include <ostream>      // std::ostream
-#include <random>       // std::random_device, std::mt19937, std::uniform_int_distribution
-#include <string_view>  // std::string_view
-#include <vector>       // std:.vector
+#include <cstddef>                 // std::size_t
+#include <ostream>                 // std::ostream
+#include <random>                  // std::random_device, std::mt19937, std::uniform_int_distribution
+#include <string_view>             // std::string_view
+#include <vector>                  // std:.vector
 
 class LoremIpsum
 {
 private:
-    std::size_t                                 m_minNumWords;
-    std::size_t                                 m_maxNumWords;
-    std::size_t                                 m_minNumSentences;
-    std::size_t                                 m_maxNumSentences;
-    std::size_t                                 m_numParagraphs;
+    std::size_t                    m_minNumWords;
+    std::size_t                    m_maxNumWords;
+    std::size_t                    m_minNumSentences;
+    std::size_t                    m_maxNumSentences;
+    std::size_t                    m_numParagraphs;
 
-    std::vector<std::string_view>               m_originalWords;
-    std::vector<std::string_view>               m_longWords;
+    std::vector<std::string_view>  m_originalWords;
+    std::vector<std::string_view>  m_longWords;
 
-    std::random_device                          m_device;
-    std::mt19937                                m_engine;
+    std::random_device             m_device;
+    std::mt19937                   m_engine;
 
     std::uniform_int_distribution<std::size_t>  m_wordDistribution;
     std::uniform_int_distribution<std::size_t>  m_wordsDistribution;
@@ -33,17 +33,22 @@ private:
 public:
     // c'tor(s)
     LoremIpsum();
-    LoremIpsum(std::size_t minNumWords, std::size_t maxNumWords, std::size_t minNumSentences, std::size_t maxNumSentences, std::size_t numParagraphs);
+    LoremIpsum(
+        std::size_t minNumWords, std::size_t maxNumWords, 
+        std::size_t minNumSentences, std::size_t maxNumSentences, 
+        std::size_t numParagraphs
+    );
 
     // public interface
-    void generateLoremIpsum                     (std::string_view fileName);
+    void generateLoremIpsum        (std::string_view fileName);
 
 private:
     // helper methods
-    void generateWord                           (std::ostream& os);
-    void generateSentence                       (std::ostream& os);
-    void generateParagraph                      (std::ostream& os);
-    void generateParagraphList                  (std::ostream& os);
+    void generateFirstWord         (std::ostream& os);
+    void generateNextWord          (std::ostream& os);
+    void generateSentence          (std::ostream& os);
+    void generateParagraph         (std::ostream& os);
+    void generateParagraphList     (std::ostream& os);
 };
 
 // =====================================================================================
--- CaseStudies/CopyOnWrite/LoremIpsumImpl.cpp
+++ CaseStudies/CopyOnWrite/LoremIpsumImpl.cpp
@@ -18,7 +18,11 @@ LoremIpsum::LoremIpsum() : LoremIpsum{ 4, 12, 5, 8, 6 }
 {
 }
 
-LoremIpsum::LoremIpsum(std::size_t minNumWords, std::size_t maxNumWords, std::size_t minNumSentences, std::size_t maxNumSentences, std::size_t numParagraphs) :
+LoremIpsum::LoremIpsum(
+    std::size_t minNumWords, std::size_t maxNumWords, 
+    std::size_t minNumSentences, std::size_t maxNumSentences,
+    std::size_t numParagraphs) 
+  :
     m_minNumWords{ minNumWords }, m_maxNumWords{ maxNumWords },
     m_minNumSentences{ minNumSentences }, m_maxNumSentences{ maxNumSentences }, m_numParagraphs{ numParagraphs },
     m_originalWords{}, m_longWords{}, 
@@ -34,31 +38,27 @@ LoremIpsum::LoremIpsum(std::size_t minNumWords, std::size_t maxNumWords, std::si
     m_sentencesDistribution = std::uniform_int_distribution<std::size_t>(minNumSentences, maxNumSentences);
 }
 
-
-//
-//LoremIpsum::LoremIpsum()
-//    : m_originalWords{}, m_longWords{}, m_device{}, m_engine{ m_device() }
-//{
-//    m_originalWords =
-//    {
-//#include "LoremIpsumOriginalWords.inl"
-//    };
-//
-//    m_wordDistribution = std::uniform_int_distribution<std::size_t>(0, m_originalWords.size() - 1);
-//    m_wordsDistribution = std::uniform_int_distribution<std::size_t>(4, 12);
-//    m_sentencesDistribution = std::uniform_int_distribution<std::size_t>(5, 8);
-//}
-
 // generates a random word
-void LoremIpsum::generateWord(std::ostream& os)
+void LoremIpsum::generateFirstWord(std::ostream& os)
 {
     // create random integer value i, uniformly distributed between 0 and max
-    //auto max{ static_cast<int>(m_originalWords.size()) - 1 };
-    //auto dist{ std::uniform_int_distribution<int>(0, max) };
-   // auto index{ dist(m_engine) };
-
     auto index{ m_wordDistribution(m_engine) };
     
+    // retrieve word from list of existing words
+    auto word{ m_originalWords.at(index) };
+
+    // word is first of current sentence, capitalize first character 
+    char upperCh{ word[0] - ('a' -  'A') };
+    auto rest{ word.substr (1)};
+
+    os << upperCh;
+    os << rest;
+}
+
+void LoremIpsum::generateNextWord(std::ostream& os)
+{
+    // create random integer value i, uniformly distributed between 0 and max
+    auto index{ m_wordDistribution(m_engine) };
 
     // retrieve word from list of existing words
     auto word{ m_originalWords.at(index) };
@@ -68,117 +68,43 @@ void LoremIpsum::generateWord(std::ostream& os)
 // generate a random sentence
 void LoremIpsum::generateSentence(std::ostream& os)
 {
-    //// create random integer value numWords,
-    //// uniformly distributed between minNumWords and maxNumWords
-    //auto dist{ std::uniform_int_distribution<std::size_t>(minNumWords, maxNumWords) };
-    //std::size_t numWords{ dist(m_engine) };
-
-    //std::vector<std::string> words;
-    //words.resize(numWords);
-
-    //// create vector of words from list of existing words
-    //std::generate(
-    //    words.begin(),
-    //    words.end(),
-    //    [&] () { return generateWord(); }
-    //);
-
-    //// include a comma for better readability :)
-    //bool includecomma{ (numWords >= (maxNumWords * 4) / 5) };
-
-    //// create a sentence of these words
-    //std::string sentence{ std::accumulate(
-    //    words.begin(),
-    //    words.end(),
-    //    std::string{},
-    //    [&, i = 0](const std::string a, const std::string& next) mutable {
-
-    //        std::string result{ a };
-    //        result += next;
-
-    //        if (includecomma && i == (numWords / 2) - 1) {
-    //            result += ",";
-    //        }
-    //        if (i != numWords - 1) {
-    //            result += " ";
-    //        }
-    //        ++i;
-    //        return result;
-    //    }
-    //) };
-
-    //sentence += ".";
-    //sentence.at(0) = std::toupper(sentence.at(0));
-    //return sentence;
-
-
-
-
-
-   // std::string result;
-
     // create random integer value numWords,
     // uniformly distributed between minNumWords and maxNumWords
-    //auto dist{ std::uniform_int_distribution<std::size_t>(minNumWords, maxNumWords) };
-    //std::size_t numWords{ dist(m_engine) };
-
-    
-
     std::size_t numWords{ m_wordsDistribution(m_engine) };
 
-    bool includecomma = (numWords >= (m_maxNumWords * 4) / 5);
-    for (std::size_t i{}; i != numWords; ++i)
-    {
-        generateWord(os);
+    generateFirstWord(os);
+    os << ' ';
 
-        if (includecomma && i == (numWords / 2) - 1)
-        {
-            //result += ",";
+    bool includecomma = (numWords >= (m_maxNumWords * 4) / 5);
+    for (std::size_t i{ 1 }; i != numWords; ++i)
+    {
+        generateNextWord(os);
+
+        if (includecomma && i == (numWords / 2) - 1) {
             os << ',';
         }
-        if (i != numWords - 1)
-        {
-            //result += " ";
+
+        if (i != numWords - 1) {
             os << ' ';
         }
     }
-   // result += ".";
+
     os << '.';
-  //  result.at(0) = std::toupper(result.at(0));
-    //return result;
 }
 
 void LoremIpsum::generateParagraph(std::ostream& os)
 {
     // create random integer value numWords,
     // uniformly distributed between minNumSentences and maxNumSentences
-    //std::uniform_int_distribution<std::size_t> dist(minNumSentences, maxNumSentences);
-    //std::size_t sentences{ dist(m_engine) };
-
-
-      std::size_t sentences{ m_sentencesDistribution(m_engine) };
-
-    //std::string result{ "\t" };
-  //  std::string result{ };
+    std::size_t sentences{ m_sentencesDistribution(m_engine) };
 
     for (std::size_t i{}; i != sentences; ++i)
     {
-        //std::string sentence{ generateSentence(minNumWords, maxNumWords) };
-        //result += sentence;
-
         generateSentence(os);
         os << ' ';
-
-       // result += " ";
-
-        //   result += generateSentence(minNumWords, maxNumWords) += " ";
     }
 
     os << '\n';
-
-   // result += "\n";
-   // os << result;
-    //   return result;
 }
 
 void LoremIpsum::generateParagraphList(std::ostream& os)
